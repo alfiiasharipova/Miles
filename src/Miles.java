bdd7b1150fbf6562864a50f9012fbb55aba91427
alfiiasharipova <alfiiasharipova@gmail.com>
2020-11-02 18:29:48 +0300

    Test case added

--- src/Miles.java
+++
@@ -1,9 +0,0 @@
-public class Miles {
-    public static void main(String[] args) {
-        int ticketPrice = 3999;
-        int mile = 1;
-        int bonusMoney = 20;
-        int bonus = bonusMoney!=0? mile*ticketPrice/bonusMoney:0;
-        System.out.println(bonus);
-    }
-}
